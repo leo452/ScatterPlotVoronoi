330886511365bf407f1df2eb9e4879913fb8b46a
leo452 <leonardo.merchan.1992@gmail.com>
2019-12-16 21:39:56 -0500

    Update scatter.js

--- scatter.js
+++ scatter.js
@@ -1,139 +1,161 @@
 async function drawScatter() {
 
-  //Access the data
-  const dataset = await d3.json(".//my_weather_data.json")
+  // 1. Access data
 
-  console.table(dataset[0])
-  const xAccessor = d=>d.dewPoint
-  const yAccessor = d=>d.humidity
+  const dataset = await d3.json("./../../../my_weather_data.json")
 
-  //establish dimension of the chart
+  const xAccessor = d => d.dewPoint
+  const yAccessor = d => d.humidity
+
+  // 2. Create chart dimensions
+
   const width = d3.min([
-    window.innerHeight *0.9,
-    window.innerWidth *0.9
+    window.innerWidth * 0.9,
+    window.innerHeight * 0.9,
   ])
-
   let dimensions = {
-    width:width,
-    height:width,
+    width: width,
+    height: width,
     margin: {
-      top:10,
-      right:10,
-      bottom:50,
-      left:50,
+      top: 10,
+      right: 10,
+      bottom: 50,
+      left: 50,
     },
   }
-  dimensions.boundedWidth = dimensions.width
-      -dimensions.margin.right
-      -dimensions.margin.left
-  dimensions.boundedHeight = dimensions.height
-      -dimensions.margin.top
-      -dimensions.margin.bottom
-  //draw canvas
+  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
 
-  const wrapper = d3.select("#wrapper").append("svg")
+  // 3. Draw canvas
+
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
       .attr("width", dimensions.width)
       .attr("height", dimensions.height)
-  
+
   const bounds = wrapper.append("g")
-      .style("transform", `translate(${
-        dimensions.margin.left
-      }px,${
-        dimensions.margin.top
-      }px)`)
+    .style("transform", `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`)
 
-  //Create Scales
-
-  const yScale = d3.scaleLinear()
-      .domain(d3.extent(dataset, yAccessor))
-      .range([dimensions.boundedHeight, 0])
-      .nice()
+  // 4. Create scales
 
   const xScale = d3.scaleLinear()
-      .domain(d3.extent(dataset, xAccessor))
-      .range([0, dimensions.boundedWidth])
-      .nice()
-  //Draw Data
-  const dots = bounds.selectAll("circle")
-      .data(dataset).enter().append("circle")
-          .attr("cx", d=>xScale(xAccessor(d)))
-          .attr("cy", d=> yScale(yAccessor(d)))
-          .attr("r", 5)
-          .attr("fill", "cornflowerblue")
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice()
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0])
+    .nice()
+
+  const drawDots = (dataset) => {
+
+    // 5. Draw data
+
+    const dots = bounds.selectAll("circle")
+      .data(dataset, d => d[0])
+
+    const newDots = dots.enter().append("circle")
+
+    const allDots = newDots.merge(dots)
+        .attr("cx", d => xScale(xAccessor(d)))
+        .attr("cy", d => yScale(yAccessor(d)))
+        .attr("r", 4)
+
+    const oldDots = dots.exit()
+        .remove()
+  }
+  drawDots(dataset)
+
+  // 6. Draw peripherals
+
+  const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+
+  const xAxis = bounds.append("g")
+    .call(xAxisGenerator)
+      .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+
+  const xAxisLabel = xAxis.append("text")
+      .attr("class", "x-axis-label")
+      .attr("x", dimensions.boundedWidth / 2)
+      .attr("y", dimensions.margin.bottom - 10)
+      .html("dew point (&deg;F)")
+
+  const yAxisGenerator = d3.axisLeft()
+    .scale(yScale)
+    .ticks(4)
+
+  const yAxis = bounds.append("g")
+      .call(yAxisGenerator)
+
+  const yAxisLabel = yAxis.append("text")
+      .attr("class", "y-axis-label")
+      .attr("x", -dimensions.boundedHeight / 2)
+      .attr("y", -dimensions.margin.left + 10)
+      .text("relative humidity")
+
+  // 7. Set up interactions
 
   const delaunay = d3.Delaunay.from(
     dataset,
-    d=>xScale(xAccessor(d)),
-    d=>yScale(yAccessor(d))
+    d => xScale(xAccessor(d)),
+    d => yScale(yAccessor(d)),
   )
   const voronoi = delaunay.voronoi()
   voronoi.xmax = dimensions.boundedWidth
   voronoi.ymax = dimensions.boundedHeight
 
   bounds.selectAll(".voronoi")
-      .data(dataset).enter().append("path")
-          .attr("class", "voronoi")
-          .attr("d", (d,i)=> voronoi.renderCell(i))
-          /* .attr("stroke", "salmon") */
-
-  //draw Periphericals
-  const xAxisGenerator = d3.axisBottom()
-      .scale(xScale)
-
-  const xAxis = bounds.append("g")
-      .call(xAxisGenerator)
-          .style("transform", `translateY(${dimensions.boundedHeight}px)`)
-
-  const yAxisGenerator = d3.axisLeft()
-      .scale(yScale)
-
-  const yAxis = bounds.append("g")
-      .call(yAxisGenerator)
-  
-  //Add interaction
-  
-  bounds.selectAll(".voronoi")
+    .data(dataset)
+    .enter().append("path")
+      .attr("class", "voronoi")
+      .attr("d", (d,i) => voronoi.renderCell(i))
+      // .attr("stroke", "salmon")
       .on("mouseenter", onMouseEnter)
       .on("mouseleave", onMouseLeave)
 
   const tooltip = d3.select("#tooltip")
-
-  function onMouseEnter(datum){
-    const dotDay = bounds.append("circle")
+  function onMouseEnter(datum) {
+    const dayDot = bounds.append("circle")
         .attr("class", "tooltipDot")
-        .attr("cx", xScale(xAccessor(datum)) )
+        .attr("cx", xScale(xAccessor(datum)))
         .attr("cy", yScale(yAccessor(datum)))
-        .attr("r",7 )
-        .style("fill", "maroon" )
+        .attr("r", 7)
+        .style("fill", "maroon")
+        .style("pointer-events", "none")
 
-    const humidityFormat = d3.format(".2f")
+    const formatHumidity = d3.format(".2f")
     tooltip.select("#humidity")
-       .text(humidityFormat(yAccessor(datum)))
+        .text(formatHumidity(yAccessor(datum)))
 
-    const dewPointFormat = d3.format(".2f")
+    const formatDewPoint = d3.format(".2f")
     tooltip.select("#dew-point")
-        .text(dewPointFormat(xAccessor(datum)))
+        .text(formatDewPoint(xAccessor(datum)))
 
-    const dateParse = d3.timeParse("%Y-%m-%d")
-    const dateFormat = d3.timeFormat("%B %A %-d,%Y")
+    const dateParser = d3.timeParse("%Y-%m-%d")
+    const formatDate = d3.timeFormat("%B %A %-d, %Y")
     tooltip.select("#date")
-        .text(dateFormat(dateParse(datum.date)))
+        .text(formatDate(dateParser(datum.date)))
 
-    const x = xScale(xAccessor(datum)) + dimensions.margin.left
-    const y = yScale(yAccessor(datum)) + dimensions.margin.top
+    const x = xScale(xAccessor(datum))
+      + dimensions.margin.left
+    const y = yScale(yAccessor(datum))
+      + dimensions.margin.top
 
     tooltip.style("transform", `translate(`
-    + `calc( -50% + ${x}px),`
-    + `calc(-100% + ${y}px)`
-    + `)`)
+      + `calc( -50% + ${x}px),`
+      + `calc(-100% + ${y}px)`
+      + `)`)
 
-    tooltip.style("opacity",1)
-  }
-  function onMouseLeave(){
-    bounds.selectAll(".tooltipDot")
-        .remove()
-    tooltip.style("opacity",0)
+    tooltip.style("opacity", 1)
   }
 
+  function onMouseLeave() {
+    d3.selectAll(".tooltipDot")
+      .remove()
+
+    tooltip.style("opacity", 0)
+  }
 }
 drawScatter()
